9f5aeafd2c4f723f21b2e7ef446a5784a0b0ab0d
samadgod569 <hellohikaisiho124@gmail.com>
2025-11-28 19:27:05 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,13 @@
-export default {
-  async fetch(req) {
-    return new Response("Worker deployed successfully!", {
-      headers: { "content-type": "text/plain" }
-    });
-  }
-};
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  // serve / as /index.html
+  let path = url.pathname === "/" ? "/index.html" : url.pathname
+  // fetch the file from your GitHub public folder
+  const file = await fetch(`https://raw.githubusercontent.com/samadgod569/Code-Mon/main/public${path}`)
+  // return the content with html content-type
+  return new Response(await file.text(), { headers: { "content-type": "text/html" } })
+}
