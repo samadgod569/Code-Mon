28a7080a6bff9fa05635469c35698f9866eb88eb
samadgod569 <hellohikaisiho124@gmail.com>
2026-03-01 10:42:42 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -52,11 +52,11 @@ if (path === "/api/agent") {
     "haiku": { model: "anthropic/claude-3.5-haiku", free: false, max_tokens: 2000 },
     "opus-4.6": { model: "anthropic/claude-opus-4.6", free: false, max_tokens: 300 },
 
-    "llama-70b": { model: "meta-llama/llama-3.1-70b-instruct", free: false, max_tokens: 32000 },
+    "llama-70b": { model: "meta-llama/llama-3.1-70b-instruct", free: false, max_tokens: 60000 },
     "nano-banana": { model: "google/gemini-3.1-flash-image-preview", free: false, max_tokens: 3000},
     "gemini-3.1-pro": { model: "google/gemini-3.1-pro-preview-customtools", free: false, max_tokens: 1700},
-    "grok-4.1": { model:"x-ai/grok-4.1-fast", free: false, max_tokens: 3000},
-    "grok-4.0": { model: "x-ai/grok-4-fast", free: false, max_tokens: 3000}
+    "grok-4.1": { model:"x-ai/grok-4.1-fast", free: false, max_tokens: 5000},
+    "grok-4.0": { model: "x-ai/grok-4-fast", free: false, max_tokens: 5000}
   };
 
   if (request.method === "GET") {
